761914bfd9d3beb596b072e65fd563929dff0289
Savin Nikolai <nikolus5000@gmail.com>
2024-11-21 18:49:02 +0300

    Done HW

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,5 +1,10 @@
+import csv
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+
+from pagination.settings import BUS_STATION_CSV
 
 
 def index(request):
@@ -7,11 +12,23 @@ def index(request):
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
+    with open(BUS_STATION_CSV, newline='', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+
+        bus_stations_ = [row for row in reader]
+
+    paginator = Paginator(bus_stations_, 10)
+
+    try:
+        page_number = int(request.GET.get("page", 1))
+    except ValueError:
+        page_number = 1
+
+    page = paginator.get_page(page_number)
 
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': page,
+        'page': page,
     }
     return render(request, 'stations/index.html', context)
+
--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 DATA = {
@@ -16,15 +17,25 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def recipe_view(request, recipe_name: dict):
+
+    servings = request.GET.get('servings', 1)
+
+    try:
+        servings = int(servings)
+        if servings <= 0:
+            raise ValueError
+    except ValueError:
+        return HttpResponse('Параметр servings должен быть положительным целым числом')
+
+    recipe = {ingredient: amount * servings for ingredient, amount in DATA[recipe_name].items()}
+
+    context = {
+        'recipe': recipe
+    }
+
+    return render(request, 'calculator/index.html', context)
+
