48354585b39654a87c7550e39ecc0b3cab80e0f4
Savin Nikolai <nikolus5000@gmail.com>
2025-02-02 15:54:35 +0300

    Add deploy

--- 3.2-crud/stocks_products/stocks_products/urls.py
+++ 3.2-crud/stocks_products/stocks_products/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from ..logistic.views import home_view
+from logistic.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
