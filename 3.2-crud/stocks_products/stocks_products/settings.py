4ca769e23154937403053a012e9263124a66d33c
Savin Nikolai <nikolus5000@gmail.com>
2025-02-18 14:48:07 +0300

    Add doker-compose

--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -80,8 +80,10 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'stocks_products_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres'
     }
 }
 
